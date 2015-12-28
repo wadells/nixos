354eda03ce4849dec8eb07e9eac4f87b5b1b758c
Walt Javins <walt@javins.net>
2015-12-28 23:26:12 +0000

    Move hardware related config from config.nix to hardware-config.nix.
    Some bluetooth/usb config disabling that really doesn't need to be in
    the main configuration.nix, as it is specific to the MBA hardware.

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -15,6 +15,31 @@
     "kvm-intel" # for nix containers
   ];
 
+  boot.blacklistedKernelModules = [
+    # Macbooks don't have PS2 capabilities, and the I8042 driver spams an err like
+    # the following on boot:
+    #
+    # Dec 26 09:43:17 nix kernel: i8042: No controller found
+    #
+    # This is harmless, but it is noise in the logs when I'm looking for real errors.
+    #
+    # Alas atkbd was built into nixpkgs here:
+    #
+    # https://github.com/NixOS/nixpkgs/commit/1c22734cd2e67842090f5d59a6c7b2fb39c1cf66
+    #
+    # so there isn't a good way to remove it from boot.kernelModules. Thus blacklisting.
+    "atkbd"
+  ];
+
+  # Bluetooth support seemed busted as of 2015-12-26
+  # journalctl was turning up messages like the follwoing every boot:
+  #
+  # Dec 26 09:11:34 nix bluetoothd[511]: Parsing /nix/store/sd154zk2qfxqd3rsbwimnaqpsca1hflg-bluez-4.101/etc/bluetooth/input.conf failed: No such file or directory
+  # Dec 26 09:11:34 nix bluetoothd[511]: Parsing /nix/store/sd154zk2qfxqd3rsbwimnaqpsca1hflg-bluez-4.101/etc/bluetooth/audio.conf failed: No such file or directory
+  # Dec 26 09:11:34 nix bluetoothd[511]: Parsing /nix/store/sd154zk2qfxqd3rsbwimnaqpsca1hflg-bluez-4.101/etc/bluetooth/serial.conf failed: No such file or directory
+  # Dec 26 09:11:34 nix bluetoothd[511]: Unknown command complete for opcode 19
+  hardware.bluetooth.enable = false;
+
   fileSystems."/" = {
     device = "/dev/disk/by-uuid/16e8f2ea-fed4-49d7-9c73-1774a2b541bd";
     fsType = "ext4";
